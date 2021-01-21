aa610ee6f737e49f27d3abce24c34c254d2dfea5
Stephen Allison <git@steve.allison.clara.co.uk>
2021-01-21 11:11:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# my-sketch
+
+A Quil sketch that draws hypocycloids (spirograph patterns).
+
+## Usage
+
+LightTable - open `core.clj` and press `Ctrl+Shift+Enter` to evaluate the file.
+
+Emacs - run cider, open `core.clj` and press `C-c C-k` to evaluate the file.
+
+REPL - run `(require 'my-sketch.core)`.
+
+## License
+
+Copyright © 2021
+
+Distributed under the Eclipse Public License either 1.0.
--- src/my_sketch/core.clj
+++ src/my_sketch/core.clj
@@ -2,6 +2,8 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def PI 3.14159)
+
 (defn hypocycloid [a b scale]
   (fn [t]
     [(* scale (+ (* (- a b) (q/cos t)) (* b (q/cos (* t (/ (- a b) b )))))) 
@@ -9,15 +11,15 @@
 
 (defn draw []
   (q/with-translation [(/ (q/width) 2) (/ (q/height) 2 )]
-    (let [t (/ (q/frame-count) 10)
-          f (hypocycloid 21 17 20)]
-      (q/line (f t) (f (+ t 0.01))))))
+    (let [t (/ (q/frame-count) 20)
+          f (hypocycloid 23 13 11)]
+      (q/line (f t) (f (+ t 0.1))))))
 
 (defn setup []
   (q/background 255)
   (q/frame-rate 60))
 
 (q/defsketch spiral
-  :size [1024 1024]
+  :size [512 512]
   :draw draw
   :setup setup)
